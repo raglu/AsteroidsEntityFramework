58f3086a682b9ac63294fd5d411fec895ddea00a
raglu16 <raglu16@student.sdu.dk>
2018-02-14 17:16:26 +0100

    Added Enemy Entity to Core

--- Core/src/main/java/dk/sdu/mmmi/cbse/main/Game.java
+++ Core/src/main/java/dk/sdu/mmmi/cbse/main/Game.java
@@ -10,6 +10,8 @@ import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
 import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+import dk.sdu.mmmi.cbse.enemysystem.EnemyControlSystem;
+import dk.sdu.mmmi.cbse.enemysystem.EnemyPlugin;
 import dk.sdu.mmmi.cbse.managers.GameInputProcessor;
 import dk.sdu.mmmi.cbse.playersystem.PlayerPlugin;
 import dk.sdu.mmmi.cbse.playersystem.PlayerControlSystem;
@@ -47,6 +49,8 @@ public class Game
         IEntityProcessingService playerProcess = new PlayerControlSystem();
         entityPlugins.add(playerPlugin);
         entityProcessors.add(playerProcess);
+        entityPlugins.add(new EnemyPlugin());
+        entityProcessors.add(new EnemyControlSystem());
         // Lookup all Game Plugins using ServiceLoader
         for (IGamePluginService iGamePlugin : entityPlugins) {
             iGamePlugin.start(gameData, world);
